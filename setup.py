6a7157fcbfa9676a705109d637e6caa6c318b6e1
lenin <lenin.ayr@gmail.com>
2010-01-20 10:37:51 -0700

    setup.py added

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.5'
+
+LONG_DESCRIPTION = """
+User accounts
+--------------------------
+"""
+
+setup(
+    name='django-accounts',
+    version=version,
+    description="django-accounts",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='accounts,django',
+    author='Lenin Yee',
+    author_email='lenin.ayr@gmail.com',
+    url='http://github.com/lenin/django-accounts',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
