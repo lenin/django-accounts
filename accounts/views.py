1ca28d4f826fa54b08735d0f3bab666659558d4c
Lenin Yee <lenin@mirra.(none)>
2010-02-23 13:57:05 -0700

    Allow extra_context in edit view: correction

--- accounts/views.py
+++ accounts/views.py
@@ -4,5 +4,5 @@ from accounts.forms import EditUserForm
 
 def edit(request, template_name='accounts/user_form.html', extra_context={}):
     return update_object(request, login_required=True, form_class=EditUserForm,
-                         object_id=request.user.id, template_name=template_name
+                         object_id=request.user.id, template_name=template_name,
                          extra_context=extra_context)
